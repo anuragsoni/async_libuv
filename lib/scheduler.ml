45ac29af49570f3364db9207a7172d784879f38a
Anurag Soni <anurag@sonianurag.com>
2021-07-28 23:21:38 -0400

    dont allocate when checking for pending events

--- lib/scheduler.ml
+++ lib/scheduler.ml
@@ -18,12 +18,13 @@ let create () =
 ;;
 
 let cycle t =
-  if Kernel_scheduler.can_run_a_job t.kernel_scheduler
-  then ()
-  else (
-    match Kernel_scheduler.next_upcoming_event t.kernel_scheduler with
-    | None -> ignore (Luv.Loop.run ~loop:t.uv_loop ~mode:`ONCE () : bool)
-    | Some _ -> ignore (Luv.Loop.run ~loop:t.uv_loop ~mode:`NOWAIT () : bool));
+  if not (Kernel_scheduler.can_run_a_job t.kernel_scheduler)
+  then
+    if (* TODO: Maybe we should always use `NOWAIT since we are attempting to get
+          async_kernel's scheduler and libuv's scheduler to play nice? *)
+       Kernel_scheduler.has_upcoming_event t.kernel_scheduler
+    then ignore (Luv.Loop.run ~loop:t.uv_loop ~mode:`NOWAIT () : bool)
+    else ignore (Luv.Loop.run ~loop:t.uv_loop ~mode:`ONCE () : bool);
   Kernel_scheduler.run_cycle t.kernel_scheduler
 ;;
 
